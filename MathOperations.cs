35e347920e8b8b8a9f800c108e038a37a11b082d
KibiAi <vetal2004v@gmail.com>
2023-10-30 18:10:35 +0200

    Добавьте файлы проекта.

---
+++ MathOperations.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6._2
+{
+    public class MathOperations
+    {
+        // Додавання
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        // Додавання масивів
+        public static double[] Add(double[] arr1, double[] arr2)
+        {
+            if (arr1.Length != arr2.Length)
+            {
+                throw new ArgumentException("Розмірності масивів не співпадають.");
+            }
+
+            double[] result = new double[arr1.Length];
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                result[i] = arr1[i] + arr2[i];
+            }
+            return result;
+        }
+
+        // Перевантаження додавання матриць
+        public static double[,] Add(double[,] matrix1, double[,] matrix2)
+        {
+            if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
+            {
+                throw new ArgumentException("Розмірності матриць не співпадають.");
+            }
+
+            int rows = matrix1.GetLength(0);
+            int columns = matrix1.GetLength(1);
+
+            double[,] result = new double[rows, columns];
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    result[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+            return result;
+        }
+
+        // Перевантаження тензорів
+        public static double[,,] Add(double[,,] tensor1, double[,,] tensor2)
+        {
+            if (tensor1.GetLength(0) != tensor2.GetLength(0) ||
+                tensor1.GetLength(1) != tensor2.GetLength(1) ||
+                tensor1.GetLength(2) != tensor2.GetLength(2))
+            {
+                throw new ArgumentException("Розмірності тензорів не співпадають.");
+            }
+
+            int depth = tensor1.GetLength(0);
+            int rows = tensor1.GetLength(1);
+            int columns = tensor1.GetLength(2);
+
+            double[,,] result = new double[depth, rows, columns];
+            for (int d = 0; d < depth; d++)
+            {
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        result[d, i, j] = tensor1[d, i, j] + tensor2[d, i, j];
+                    }
+                }
+            }
+            return result;
+        }
+
+        // Перевантаження віднімання
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        // Перевантаження множення
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using lab6._2;
+
+class Program
+{
+    static void Main()
+    {
+        double a = 7.0;
+        double b = 3.0;
+
+        double result1 = MathOperations.Add(a, b);
+        Console.WriteLine($"Додавання чисел: {result1}");
+
+        double[] arr1 = { 1.0, 2.0, 3.0 };
+        double[] arr2 = { 4.0, 5.0, 6.0 };
+
+        double[] result2 = MathOperations.Add(arr1, arr2);
+        Console.WriteLine($"Додавання масивів: [{string.Join(", ", result2)}]");
+
+        double[,] matrix1 = { { 1.0, 2.0 }, { 3.0, 4.0 } };
+        double[,] matrix2 = { { 5.0, 6.0 }, { 7.0, 8.0 } };
+
+        double[,] result3 = MathOperations.Add(matrix1, matrix2);
+        Console.WriteLine("Додавання матриці:");
+        for (int i = 0; i < result3.GetLength(0); i++)
+        {
+            for (int j = 0; j < result3.GetLength(1); j++)
+            {
+                Console.Write($"{result3[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+        double[,,] tensor1 = { { { 1.0, 2.0 }, { 3.0, 4.0 } }, { { 5.0, 6.0 }, { 7.0, 8.0 } } };
+        double[,,] tensor2 = { { { 9.0, 10.0 }, { 11.0, 12.0 } }, { { 13.0, 14.0 }, { 15.0, 16.0 } } };
+
+        double[,,] result4 = MathOperations.Add(tensor1, tensor2);
+        Console.WriteLine("Додавання тензора:");
+        for (int d = 0; d < result4.GetLength(0); d++)
+        {
+            for (int i = 0; i < result4.GetLength(1); i++)
+            {
+                for (int j = 0; j < result4.GetLength(2); j++)
+                {
+                    Console.Write($"{result4[d, i, j]} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        double result5 = MathOperations.Subtract(a, b);
+        Console.WriteLine($"Віднімання чисел: {result5}");
+
+        double result6 = MathOperations.Multiply(a, b);
+        Console.WriteLine($"Множення чисел: {result6}");
+    }
+}
